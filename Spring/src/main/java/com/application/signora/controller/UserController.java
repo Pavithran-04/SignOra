9d423fa556455c2095a357d35680c0724971b200
Pavithran-04 <pavithranganesan04@gmail.com>
2026-02-08 00:08:12 +0530

    CREATE: Adding the login feature

--- Spring/src/main/java/com/application/signora/controller/UserController.java
+++ Spring/src/main/java/com/application/signora/controller/UserController.java
@@ -1,6 +1,10 @@
 package com.application.signora.controller;
 
-import com.application.signora.dto.request.user.RegisterUserRequest;
+import com.application.signora.dto.request.LoginUserRequest;
+import com.application.signora.dto.request.RegisterUserRequest;
+import com.application.signora.dto.request.student.FormRequest;
+import com.application.signora.dto.response.student.FormResponse;
+import com.application.signora.dto.response.user.LoginResponse;
 import com.application.signora.dto.response.user.RegisterUserResponse;
 import com.application.signora.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,9 +16,14 @@ public class UserController {
     @Autowired
     private UserService service;
 
-    @PostMapping("/user/register")
+    @PostMapping("/admin/user/register")
     public RegisterUserResponse createUser(@RequestBody RegisterUserRequest request) {
         return service.registerUser(request);
+    }
+
+    @PostMapping("/login")
+    public LoginResponse loginUser(@RequestBody LoginUserRequest loginUserRequest) {
+        return service.loginUser(loginUserRequest);
     }
 
 }
