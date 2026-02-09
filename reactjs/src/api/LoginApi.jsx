107dd0afb5df86f18e5bf33dd7d70d1e21111c16
nivetha.kalaimani <nivetha.kalaimani@onebillsoftware.com>
2026-02-09 22:09:03 +0530

    Fix:connection

--- reactjs/src/api/LoginApi.jsx
+++ reactjs/src/api/LoginApi.jsx
@@ -1,15 +1,12 @@
 import axiosInstance from "./CommonUrl";
 
 export const loginUser = async (credentials) => {
+  localStorage.removeItem("accessToken");
   const response = await axiosInstance.post("/login", credentials);
-  console.log("response");
   return response.data;
 };
 
 export const accessToken = async (credentials) => {
-  console.log("credentials", credentials);
-  localStorage.removeItem("accessToken");
-
   const response = await axiosInstance.post("/auth/token", credentials);
   return response.data;
 };
