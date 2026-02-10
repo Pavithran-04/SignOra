2372d957e3b811c6ef7cc939306ead63eae3ae22
Pavithran-04 <pavithranganesan04@gmail.com>
2026-02-10 22:20:51 +0530

    CREATE: API for Department creation and get them by college id

---
+++ Spring/src/main/java/com/application/signora/dto/response/department/DepartmentInfo.java
@@ -0,0 +1,18 @@
+package com.application.signora.dto.response.department;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class DepartmentInfo {
+
+    private Long id;
+
+    private String name;
+
+}
--- Spring/src/main/java/com/application/signora/repository/DepartmentRepository.java
+++ Spring/src/main/java/com/application/signora/repository/DepartmentRepository.java
@@ -2,8 +2,11 @@ package com.application.signora.repository;
 
 import com.application.signora.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,5 +15,6 @@ public interface DepartmentRepository extends JpaRepository<Department, Long> {
     boolean existsByName(String departmentName);
 
     Optional<Department> findByName(String departmentName);
-
+    
+    List<Department> findAllByCollegeId(Long collegeId);
 }
