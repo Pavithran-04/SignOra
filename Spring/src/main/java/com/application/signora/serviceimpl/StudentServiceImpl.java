f231466ea96819bb425113bf220eaf16c66524d4
Pavithran-04 <pavithranganesan04@gmail.com>
2026-02-14 14:03:06 +0530

    CREATE: Student entity creationg and Registration

--- Spring/src/main/java/com/application/signora/serviceimpl/StudentServiceImpl.java
+++ Spring/src/main/java/com/application/signora/serviceimpl/StudentServiceImpl.java
@@ -1,23 +1,20 @@
 package com.application.signora.serviceimpl;
 
-import com.application.signora.config.CustomUserDetails;
 import com.application.signora.dto.request.student.CreateStudentRequest;
 import com.application.signora.dto.request.student.FormRequest;
+import com.application.signora.dto.response.student.CreateStudentResponse;
 import com.application.signora.dto.response.student.FormResponse;
-import com.application.signora.entity.ApprovalInfo;
-import com.application.signora.entity.CertificateInfo;
-import com.application.signora.entity.RequestDetails;
+import com.application.signora.entity.*;
 import com.application.signora.entity.enums.RequestStatus;
+import com.application.signora.entity.enums.UserType;
 import com.application.signora.repository.*;
 import com.application.signora.service.StudentService;
 import com.application.signora.utility.StudentServiceUtil;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class StudentServiceImpl implements StudentService {
@@ -40,10 +37,57 @@ public class StudentServiceImpl implements StudentService {
     @Autowired
     CertificateInfoRepository certificateInfoRepository;
 
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    PasswordEncoder encoder;
+
+    @Autowired
+    BatchDetailsRepository batchDetailsRepository;
+
+    @Autowired
+    CollegeRepository collegeRepository;
+
     @Override
     @Transactional
-    public void createStudent(CreateStudentRequest request) {
+    public CreateStudentResponse createStudent(CreateStudentRequest request) {
+        if (studentRepository.existsByRollNo(request.getRollNo())) {
+            throw new RuntimeException("Roll number already exists");
+        }
 
+        if (userRepository.existsByUsername(request.getUsername())) {
+            throw new RuntimeException("Username already exists");
+        }
+
+        User savedUser = userRepository.save(User.builder()
+                .username(request.getUsername())
+                .password(encoder.encode(request.getPassword()))
+                .role(UserType.STUDENT.toString())
+                .build());
+
+        BatchDetails batchDetails = batchDetailsRepository.findById(request.getBatchId()).get();
+        Department department = departmentRepository.findById(batchDetails.getDepartment().getId()).get();
+        College college = collegeRepository.findById(department.getCollege().getId()).get();
+
+        Student savedStudent = studentRepository.save(Student.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .rollNo(request.getRollNo())
+                .batchDetails(batchDetails)
+                .user(savedUser)
+                .build());
+
+        return CreateStudentResponse.builder()
+                .id(savedStudent.getId())
+                .firstName(savedStudent.getFirstName())
+                .lastName(savedStudent.getLastName())
+                .rollNo(savedStudent.getRollNo())
+                .username(savedUser.getUsername())
+                .collegeId(college.getId())
+                .departmentId(department.getId())
+                .batchId(batchDetails.getId())
+                .build();
     }
 
     @Override
